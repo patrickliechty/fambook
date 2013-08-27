a8382f37324c299cff1888815efb0bc0e1d10ad0
Patrick Liechty <liechtypo@familysearch.org>
2013-08-27 09:54:18 -0600

    Comment out socket.io

--- app.js
+++ app.js
@@ -42,16 +42,16 @@ app.configure('development', function() {
   });
 });
 
-var io = require('socket.io').listen(app);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-
-  socket.on('disconnect', function () {
-    console.log("user disconnected");
-  });
-});
+//var io = require('socket.io').listen(app);
+//
+//io.sockets.on('connection', function (socket) {
+//  socket.emit('news', { hello: 'world' });
+//  socket.on('my other event', function (data) {
+//    console.log(data);
+//  });
+//
+//  socket.on('disconnect', function () {
+//    console.log("user disconnected");
+//  });
+//});
 
